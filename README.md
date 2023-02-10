72b69559a0572b27ee960aa85490a6020712c53c
lexymolina <lexymato@colegioguanenta.edu.co>
2023-02-10 09:53:30 -0500

    se modificaron el README y conversor_temperatura.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # conversor_temperatura
 Programa para convertir una cantidad dada de grados centigrados a su equivalente en Farenheit y Kelvin
 
-## Diagrama de flujo
+ ## Diagrama de flujo 
 
 ![Diagrama de flujo](diagrama.png "Diagrama de flujo" )
--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -4,7 +4,15 @@ print("--conversor temperatura--")
 print("-------------------------")
 
 # input
-
+C = int(input("digite el valor de c: "))
 
 # processing
+F = C*1.8 + 32
+K = C + 273.15
 
+#output
+print("--------------------")
+print("-----resultados-----")
+print("--------------------")
+
+print("")
